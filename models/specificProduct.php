3a42b188552a77bcceb2c2e8fa7c5d0fc74026e5
dieromeo <diegoromeo99@icloud.com>
2024-01-16 15:35:38 +0100

    feat: specific product model

---
+++ models/specificProduct.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . "/product.php";
+
+class SpecificProduct extends Product
+{
+    public $tipology;
+
+    function __construct($_name, $_price, $_category, $_description, $_tipology)
+    {
+        parent::__construct($_name, $_price, $_category, $_description);
+        $this->setTipology($_tipology);
+    }
+
+    /**
+     * setTipology setta la tipologia se presete nell'array
+     *
+     * @param  mixed $tipology
+     * @return void
+     */
+    public function setTipology($tipology)
+    {
+        $tipology_value = ["cibo", "cuccia", "giochi"];
+        $tipology_lower = strtolower($tipology);
+        if (in_array($tipology_lower, $tipology_value)) {
+            $this->tipology = $tipology;
+        }
+    }
+}
